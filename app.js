bac96f8a47b2e4fc74696839a8e1853ca51c3395
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-01-18 16:58:24 -0800

    complete css start javascript

---
+++ app.js
@@ -0,0 +1,44 @@
+var tesks = [];
+
+const addTask = () => {
+    const teskInput = document.getElementById("teskInput")
+    const text = teskInput.value.trim()
+
+    if (text) {
+        tesks.push({ text: text, completed: false })
+
+        updateTesksList()
+    }
+};
+
+const updateTesksList = () => {
+    const tesklist = document.getElementById("tesk-list")
+    tesklist.innerHTML = ""
+
+    tesks.forEach((tesk, index) => {
+        const listItem = document.createElement("li")
+
+        listItem.innerHTML = `
+    
+        <div class="teskItem">
+            <div class="tesk  ${tesk.completed ? 'completed' : ''} ">
+                <input type="checkbox" class="checkbox" ${tesk.completed ? 'checked' : ""} />
+                <p>${tesk.text}</p>
+            </div>
+            <div class="icons">
+                <img src="./img/edit.png" onclick="editTask(${index})" />
+                <img src="./img/bin.png"  onclick="deleteTask(${index})" />
+            </div>
+        </div>
+         
+         `
+        listItem.addEventListener("change", () => toggleTaskcomplete(index))
+        tesklist.append(listItem);
+    })
+}
+
+document.getElementById("newTask").addEventListener("click", function (e) {
+    e.preventDefault()
+
+    addTask()
+})
